42ff4350c78310a5a6fc82f16e4c9d8f5a0b7eec
asena-98 <asena.akbas@capgemini.com>
2026-06-15 16:08:08 +0200

    Improved booking response validations

--- src/test/java/com/booking/stepdefinitions/BookingSteps.java
+++ src/test/java/com/booking/stepdefinitions/BookingSteps.java
@@ -15,6 +15,7 @@ import java.time.LocalDate;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 public class BookingSteps {
 
@@ -25,6 +26,7 @@ public class BookingSteps {
     private int bookingId;
     private String existingBookingJson;
     private String createdBookingJson;
+    private String updatedBookingJson;
 
     @When("I check the booking API health")
     public void iCheckTheBookingApiHealth() {
@@ -52,7 +54,10 @@ public class BookingSteps {
     @Then("the booking should be created successfully")
     public void theBookingShouldBeCreatedSuccessfully() {
         JsonPath expectedBooking = JsonPath.from(createdBookingJson);
-        response.then().statusCode(201).body("bookingid", notNullValue())
+
+        response.then().statusCode(201)
+                .body(matchesJsonSchemaInClasspath("schemas/create-booking-response-schema.json"))
+                .body("bookingid", notNullValue())
                 .body("roomid", equalTo(expectedBooking.getInt("roomid")))
                 .body("firstname", equalTo(expectedBooking.getString("firstname")))
                 .body("lastname", equalTo(expectedBooking.getString("lastname")))
@@ -91,9 +96,16 @@ public class BookingSteps {
 
     @Then("the booking details should be returned")
     public void theBookingDetailsShouldBeReturned() {
-        response.then().statusCode(200).body("firstname", notNullValue())
-                .body("lastname", notNullValue()).body("bookingdates.checkin", notNullValue())
-                .body("bookingdates.checkout", notNullValue());
+        JsonPath expectedBooking = JsonPath.from(existingBookingJson);
+
+        response.then().statusCode(200).body("roomid", equalTo(expectedBooking.getInt("roomid")))
+                .body("firstname", equalTo(expectedBooking.getString("firstname")))
+                .body("lastname", equalTo(expectedBooking.getString("lastname")))
+                .body("depositpaid", equalTo(expectedBooking.getBoolean("depositpaid")))
+                .body("bookingdates.checkin",
+                        equalTo(expectedBooking.getString("bookingdates.checkin")))
+                .body("bookingdates.checkout",
+                        equalTo(expectedBooking.getString("bookingdates.checkout")));
     }
 
     @When("I update the booking")
@@ -105,14 +117,23 @@ public class BookingSteps {
         LocalDate checkout = checkin.plusDays(2);
         bookingJson = bookingJson.replace("${checkin}", checkin.toString()).replace("${checkout}",
                 checkout.toString());
+        updatedBookingJson = bookingJson;
         response = bookingApi.updateBooking(bookingId, token, bookingJson);
     }
 
     @Then("the booking should be updated successfully")
     public void theBookingShouldBeUpdatedSuccessfully() {
-        response.then().statusCode(200).body("booking.firstname", equalTo("Updated"))
-                .body("booking.lastname", equalTo("Booking"))
-                .body("booking.depositpaid", equalTo(false));
+        JsonPath expectedBooking = JsonPath.from(updatedBookingJson);
+
+        response.then().statusCode(200).body("bookingid", equalTo(bookingId))
+                .body("booking.roomid", equalTo(expectedBooking.getInt("roomid")))
+                .body("booking.firstname", equalTo(expectedBooking.getString("firstname")))
+                .body("booking.lastname", equalTo(expectedBooking.getString("lastname")))
+                .body("booking.depositpaid", equalTo(expectedBooking.getBoolean("depositpaid")))
+                .body("booking.bookingdates.checkin",
+                        equalTo(expectedBooking.getString("bookingdates.checkin")))
+                .body("booking.bookingdates.checkout",
+                        equalTo(expectedBooking.getString("bookingdates.checkout")));
     }
 
     @When("I delete the booking")
